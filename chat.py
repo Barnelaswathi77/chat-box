c78ebe381d02876adb911df75161f29e7fec3c06
Barnelaswathi77 <103959491+Barnelaswathi77@users.noreply.github.com>
2022-08-18 15:06:55 +0530

    Add files via upload

---
+++ chat.py
@@ -0,0 +1,31 @@
+a=input("enter the text:")
+if(a=="hi"):
+    print("hello")
+    print("enter the text")
+    b=input("enter the text:")
+    if(b=="how are you"):
+        print("iam good")
+        print("enter the text")
+        c=input("enter the text:")
+        if(c=="what are you doing on studies"):
+            print("iam coding")
+            print("enter the text:")
+            d=input("enter the text")
+            if(d=="shall we meet"):
+                print("yeah,sure")
+                print("enter the text")
+                e=input("enter the text:")
+                if(e=="meet me at 2 pm"):
+                    print("ok bye")
+                    print("enter the text")
+                else:
+                    print("empty")
+            else:
+                print("empty")
+        else:
+            print("empty")
+    else:
+        print("empty")
+else:
+    print("empty")
+
